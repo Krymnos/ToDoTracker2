ed194c07381b2cad50330f835576636d44459524
NatalieKa <n.kaminsky@campus.tu-berlin.de>
2015-05-06 23:40:39 +0200

    Another try
    Another try to make "Edit Task" work - current error '404 - Page not
    found'

--- toDo/toDoTracker/forms.py
+++ toDo/toDoTracker/forms.py
@@ -4,3 +4,8 @@ class NewTaskForm(forms.Form):
     task = forms.CharField(max_length=160)
     progress = forms.IntegerField()
     deadline = forms.DateTimeField()
+
+class EditTaskForm(forms.Form):
+    task = forms.CharField(max_length=160)
+    progress = forms.IntegerField()
+    deadline = forms.DateTimeField()
--- toDo/toDoTracker/urls.py
+++ toDo/toDoTracker/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, url
 from . import views
-from django.views.generic import UpdateView
+from django.views.generic.edit import UpdateView
+from toDoTracker.models import List
 
 
 urlpatterns = patterns('',
     #url(r'^$', views.IndexView.as_view()),
     url(r'^$', 'toDoTracker.views.index'), #name='index'),
-    #url(r'^.*Edit-Task.html', views.EditTaskView.as_view()),
-    url(r'^myobject/update/(?P<pk>\w+)?$', views.UpdateView.as_view(model=myModel, name='edit_task')),
+    url(r'^Edit-Task.html', views.EditTaskView.as_view()),
+    #url('^to_do_update/(?P<pk>[\w-]+)$', views.EditTaskView.as_view()),
     #url(r'^.*New-Task.html', views.NewTaskView.as_view()),
     url(r'^Impressum.html', views.ImpressumView.as_view()),
     url(r'^New-Task.html',
