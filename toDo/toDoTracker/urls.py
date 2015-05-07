efc5ca32ec9327c5f640727b79e6b90f609c6da6
Ron W <ron.w939@gmail.com>
2015-05-07 23:56:46 +0200

    delete button added
    delete button added

--- toDo/toDoTracker/urls.py
+++ toDo/toDoTracker/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     url(r'^edit/(?P<pk>\d+)$', views.TaskUpdateView.as_view(), name='edit_task'),
     url(r'^New-Task.html', views.ToDoCreate.as_view(), name = 'new_task'),
     url(r'^Impressum.html', views.ImpressumView.as_view()),
-    url(r'^toDoTracker/(?P<pk>[0-9]+)/$', views.finishTask, name='finishTask')
+    url(r'^toDoTracker/finish/(?P<pk>[0-9]+)$', views.finishTask, name='finishTask'),
+    url(r'^toDoTracker/delete/(?P<pk>[0-9]+)$', views.deleteTask, name='deleteTask')
     
     )
